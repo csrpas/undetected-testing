bfe034e2869f3f831afb70458978e060be31356c
csrpas <adhominem17@gmail.com>
2025-01-17 20:19:17 +0100

    Update raw_gt.py

--- raw_gt.py
+++ raw_gt.py
@@ -4,9 +4,9 @@ with SB(uc=True, test=True, locale_code="en") as sb:
     url = "https://www.guatecompras.gt/proveedores/busquedaProvee.aspx"
     sb.activate_cdp_mode(url)
     sb.press_keys("#MasterGC_ContentBlockHolder_txtNit", "87560100")
-    sb.sleep(1)
+    sb.sleep(10)
     sb.uc_click('input#MasterGC_ContentBlockHolder_cmdNit')
-    sb.sleep(2)
+    sb.sleep(20)
     sb.uc_gui_click_captcha()
-    sb.sleep(2)
+    sb.sleep(10)
     print(sb.get_text("//*[@id='MasterGC_ContentBlockHolder_lblNombreProv']"))
