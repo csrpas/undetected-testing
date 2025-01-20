ed43b8d0f1576dbbb6e365fff5997e37834d0191
csrpas <adhominem17@gmail.com>
2025-01-20 15:25:08 +0100

    Update raw_gt_2.py

--- raw_gt_2.py
+++ raw_gt_2.py
@@ -3,9 +3,9 @@ from seleniumbase import SB
 with SB(uc=True, test=True, locale_code="en") as sb:
     url = "https://www.guatecompras.gt/proveedores/busquedaProvee.aspx"
     sb.activate_cdp_mode(url)
-    sb.press_keys("#MasterGC_ContentBlockHolder_txtNit", "87560100")
+    sb.cdp.press_keys("#MasterGC_ContentBlockHolder_txtNit", "87560100")
     sb.sleep(10)
-    sb.cdp.uc_click('input#MasterGC_ContentBlockHolder_cmdNit')
+    sb.cdp.click('input#MasterGC_ContentBlockHolder_cmdNit')
     sb.sleep(20)
     sb.cdp.gui_click_element("#MasterGC_ContentBlockHolder_divCaptcha div")
     # sb.uc_gui_click_captcha()
