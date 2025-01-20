650d9b8d528c8bca9fc167432e829ca2253daaff
csrpas <adhominem17@gmail.com>
2025-01-20 16:32:07 +0100

    Update raw_planetmc.py

--- raw_planetmc.py
+++ raw_planetmc.py
@@ -1,8 +1,8 @@
 from seleniumbase import SB
 
-with SB(uc=True, test=True) as sb:
-    url = "www.planetminecraft.com/account/sign_in/"
+with SB(uc=True, test=True, locale_code="en") as sb:
+    url = "https://gitlab.com/users/sign_in"
     sb.activate_cdp_mode(url)
+    sb.uc_gui_click_captcha()
     sb.sleep(2)
-    sb.cdp.gui_click_element("#turnstile-widget div")
-    sb.sleep(2)
+    print(sb.get_text("label[for='user_login']"))
