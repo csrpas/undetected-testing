90e8f289cddfacf1ce8c0d3491dacfea2dc481da
csrpas <adhominem17@gmail.com>
2025-01-21 11:59:21 +0100

    Update my_socialblade_2.py

--- my_socialblade_2.py
+++ my_socialblade_2.py
@@ -6,5 +6,7 @@ with SB(uc=True, test=True, ad_block=True, pls="none") as sb:
     sb.sleep(2)
     sb.uc_gui_click_captcha()
     sb.sleep(1)
-    print(sb.get_text('h2[style="padding-top: 20px; color:#cc6767; font-size: 1.2em;"]'))
+    print(
+        sb.get_text('h2[style="padding-top: 20px; color:#cc6767; font-size: 1.2em;"]')
+    )
     print(sb.get_text('h2[style="font-size: 1.1em; color:#ff4c4c;"]'))
